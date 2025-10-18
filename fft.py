6322ea49164f11e03d5d297d16ecb18f1c93ab41
rda12345 <roiedann1987@gmail.com>
2025-10-18 09:00:41 +0300

    Add files via upload
    Added an implementation of Cooley–Tukey FFT algorithm

---
+++ fft.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+An implementation of the radix-2 recursive Cooley–Tukey FFT algorithm.
+Assumes the length of the input array is a power of 2.
+Input and output are numpy arrays. 
+
+This is a divde and conquer algorithm, where the recursion relation is based
+on the a seperation to two subrountines on the even and odd indicies, respectively,
+and a reuse of the evaluated results. 
+
+Complexity: O(N*log(N))
+"""
+import numpy as np
+
+
+def fft(x):
+    """
+    Computes the Discrete Fourier Transform (DFT) of x 
+    using the recursive Cooley–Tukey FFT algorithm. Input and output are numpy arrays.
+    
+    Parameters: 
+        x: np.ndarray, input signal (length must be of power of 2) 
+    
+    Returns:
+        X: np.ndarray: DFT of x     
+    """
+    N = len(x)
+    
+    if len(x) ==1: return x
+    elif len(x) == 2:
+        return np.array([x[0]+x[1], x[0]-x[1]])
+
+    
+    E = fft(x[::2])  # fft on all even elements of x
+    O = fft(x[1::2]) # fft on all odd elements of x
+    
+    k = np.arange(0,N//2)     # fourier index vector
+    W = np.exp(-1j*2*np.pi*k/N) # phases
+    
+    X_low = E + W*O     # DFT of elements k = [0,...,N/2-1] (using elementwise multipication of the phases and odd terms)
+    X_high = E - W*O    # DFT of elements k = [N/2,...,N] 
+    X = np.concatenate((X_low, X_high)) 
+    return X
+    
+def fftshift(x):
+    """
+    Performs an FFT shift operation.
+
+    Rearranges the output of an FFT so that the zero-frequency component 
+    is centered in the array. 
+
+    Originally, the frequency bins are ordered as: [0, 1, 2, ..., N/2 - 1, -N/2, ..., -1]
+    After the shift, they are reordered to: [-N/2, ..., -1, 0, 1, ..., N/2 - 1]
+    
+    Parameters:
+        x: np.ndarray, input array
+        
+    Returns:
+        np.ndarray, shifted array
+    """
+    N = len(x)
+    return np.concatenate((x[N//2:],x[:N//2]))
+
+
+## Test
+import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    L = 1
+    sig = L/10
+    N = 2**6
+    x = np.linspace(-L,L,N)
+    k = np.arange(-N/2,N/2)*np.pi/L
+    y = np.exp(-(x**2)/(2*sig**2))/(np.sqrt(2*np.pi*sig**2))
+    
+    fft = fft(y)
+    shifted_fft = fftshift(fft) 
+    plt.plot(k,np.abs(shifted_fft))
+    plt.xlabel("k")
+    plt.ylabel("|fft(x)|")
+    plt.title("Absolute value of the DFT of a Gaussian")
+    plt.grid(True)
+    plt.show()
+    
+    
+    
+    
+    
