a5df9af5bbd83938f3f319d4b0e3a53b396f0c72
rda12345 <roiedann1987@gmail.com>
2026-01-05 23:07:49 +0200

    added Nestrov accelerated gradient

--- optimization/gradient_based_methods/momentum.py
+++ optimization/gradient_based_methods/momentum.py
@@ -1,16 +1,17 @@
 """
-Momentum algorithm, a.k.a heavy-ball method (Polyak, 1964)
+Momentum optimization algorithms
+
+The file includes:
+- momentum
+- Nestrov accelerated gradient (NAG)
+- Adagrad
+- RMSprop
 
 Motivation: standard gradient descent fluctuates in narrow valleys,
 and may suffer from slow convergence when gradients point in similar directions.
 Momentum fixes these issues by accumulating past gradients.
 It accelerates in consistent directions, smooths out oscillations and escapes
 shallow minima faster.
-
-When f is L-smooth and mu-strongly convex, one can define optimal learning and
-momentum rates:
-eta = 4/(sqrt(L) + sqrt(mu))
-beta = [(sqrt(L) - sqrt(mu)/(sqrt(L) + sqrt(mu)]^2
 """
 
 
@@ -39,36 +40,84 @@ def momentum(
     Args:
         objective_function (function): the objective function
         gradient (function): evaluates the gradient of objective function at point x
-        eta (float): the learning rate
-        beta (float): the momentum factor
+        eta (float): the learning rate, controls the step size
+        beta (float): the momentum factor, controls how much of the past
+                    gradients are remembered in the current iteration
         x (np.array): initial search point
 
     Returns:
         float: new search point
     """
-    v_new = beta * v + gradient(objective_function, x)
+    v_new = beta * v + (1 - beta) * gradient(objective_function, x)
     x_new = x - eta * v_new
 
     return x_new, v_new
 
+def NAG(
+        objective_function,
+        gradient,
+        eta: float,
+        beta: float,
+        x: float,
+        v: float
+) -> float:
+    """
+    Nestrov accelerated gradient (NAG)
+    Modifies the standard update rule by calculating the gradient
+    at the upcoming position. Considered more efficient due to
+    a better understanding of the future trajectory, leading to
+    faster convergence, in some cases.
+    """
+    v_new = beta * v + (1 - beta) * gradient(objective_function, x - eta * v)
+    x_new = x - eta * v_new
+
+    return x_new, v_new
+
+# TODO: code adagrad
+def adagrad(
+        objective_function,
+        gradient,
+        eta: float,
+) -> float:
+
+# TODO: code RMSprop
+def RMSprop(
+        objective_function,
+) -> float:
+
+
+
+
 
 if __name__ == "__main__":
-    x = 0.0
-    v = 0.0
-    vec = [1-x]
+    vec = [1]
+    vecNAG = vec.copy()
+
+    # momentum
+    x, v = 0.0, 0.0
     for _ in range(100):
         x_new, v_new = momentum(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
         x = x_new
         v = v_new
         vec.append(np.abs(1-x))
 
+    print(f"Error: {np.abs(x_new - 1) * 100}%")
 
-    print(f"Error: {np.abs(x_new-1)*100}%")
+    # Nestrov
+    x, v = 0.0, 0.0
+    for _ in range(100):
+        x_new, v_new = NAG(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
+        x = x_new
+        v = v_new
+        vecNAG.append(np.abs(1-x))
+
 
     plt.figure()
     plt.plot(vec)
+    plt.plot(vecNAG)
     plt.xlabel("Iteration")
     plt.ylabel("error")
+    plt.legend(["momentum","Nestrov"])
     plt.show()
 
 
