7a9347cc151425fb5acd1a4e9e012ea056379360
rda12345 <roiedann1987@gmail.com>
2026-01-09 07:12:21 +0200

    modified momentum

--- optimization/gradient_based_methods/momentum.py
+++ optimization/gradient_based_methods/momentum.py
@@ -35,7 +35,9 @@ def momentum(
         eta: float,
         beta: float,
         x: np.array,
-        v: np.array
+        v: np.array,
+        max_iter: int = 100,
+        threshold: float = 1e-6
 ) -> np.array:
     """
     Evaluates a new search point by momentum algorithm aiming to converge to the
@@ -48,14 +50,22 @@ def momentum(
         beta (float): the momentum factor, controls how much of the past
                     gradients are remembered in the current iteration
         x (np.array): initial search point
+        max_iter (int): maximum number of iterations
+        threshold (float): convergence threshold
 
     Returns:
         np.ndarray: new search point
     """
-    v_new = beta * v + (1 - beta) * gradient(objective_function, x)
-    x_new = x - eta * v_new
-
-    return x_new, v_new
+    history = []
+    for i in range(max_iter):
+        v_new = beta * v + (1 - beta) * gradient(objective_function, x)
+        x_new = x - eta * v_new
+        history.append(x_new)
+        if np.linalg.norm(x_new-x) < threshold:
+            return x_new, history
+        x = x_new
+        v = v_new
+    return x_new, history
 
 def NAG(
         objective_function,
@@ -63,7 +73,9 @@ def NAG(
         eta: float,
         beta: float,
         x: np.array,
-        v: np.array
+        v: np.array,
+        max_iter: int = 100,
+        threshold: float = 1e-6
 ) -> np.array:
     """
     Nestrov accelerated gradient (NAG)
@@ -72,10 +84,16 @@ def NAG(
     a better understanding of the future trajectory, leading to
     faster convergence, in some cases.
     """
-    v_new = beta * v + (1 - beta) * gradient(objective_function, x - eta * v)
-    x_new = x - eta * v_new
-
-    return x_new, v_new
+    history = []
+    for _ in range(max_iter):
+        v_new = beta * v + (1 - beta) * gradient(objective_function, x - eta * v)
+        x_new = x - eta * v_new
+        history.append(x_new)
+        if np.linalg.norm(x_new-x) < threshold:
+            return x_new, history
+        x = x_new
+        v = v_new
+    return x_new, history
 
 
 def adagrad(
@@ -85,6 +103,8 @@ def adagrad(
         eta: float,
         eps: float,
         x: np.array,
+        max_iter: int = 100,
+        threshold: float = 1e-6
 ) -> float:
     """
     Adagrad optimization algorithm. Adjusts the learning
@@ -97,18 +117,27 @@ def adagrad(
         eta (float): global learning rate
         eps (float): small value added to avoid divergence of the learning rate
         x (np.array): search point
+        max_iter (int): maximum number of iterations
+        threshold (float): convergence threshold
 
     Returns:
         np.ndarray: new search point
     """
-    if np.linalg.norm(G) < eps:
-        learning_rate = eta
-    else:
-        learning_rate = eta / np.sqrt(G + eps)
-    g = gradient(objective_function, x)
-    x_new = x - learning_rate * g
-    G += g**2
-    return x_new, G
+    history = []
+    for i in range(max_iter):
+        if np.linalg.norm(G) < eps:
+            learning_rate = eta
+        else:
+            learning_rate = eta / np.sqrt(G + eps)
+        g = gradient(objective_function, x)
+        x_new = x - learning_rate * g
+        G += g**2
+        history.append(x_new)
+        if np.linalg.norm(x_new - x) < threshold:
+            return x_new, history
+        x = x_new
+
+    return x_new, history
 
 
 
@@ -121,6 +150,8 @@ def RMSprop(
         eps: float,
         beta: float,
         x: np.array,
+        max_iter: int = 100,
+        threshold: float = 1e-6
 ) -> float:
     """
     RMSprop optimization algorithm. Adjusts the learning rate
@@ -135,19 +166,27 @@ def RMSprop(
         eps (float): small value added to avoid divergence of the learning rate
         beta (float): controls the memory of the moving average
         x (np.array): search point
+        max_iter (int): maximum number of iterations
+        threshold (float): convergence threshold
 
 
     Returns:
         np.ndarray: new search point
     """
-    if np.linalg.norm(G) < eps:
-        learning_rate = eta
-    else:
-        learning_rate = eta / np.sqrt(G + eps)
-    g = gradient(objective_function, x)
-    x_new = x - learning_rate * g
-    G = beta * G + (1 - beta) * g**2
-    return x_new, G
+    history = []
+    for i in range(max_iter):
+        if np.linalg.norm(G) < eps:
+            learning_rate = eta
+        else:
+            learning_rate = eta / np.sqrt(G + eps)
+        g = gradient(objective_function, x)
+        x_new = x - learning_rate * g
+        G = beta * G + (1 - beta) * g**2
+        history.append(x_new)
+        if np.linalg.norm(x_new - x) < threshold:
+            return x_new, history
+        x = x_new
+    return x_new, history
 
 
 if __name__ == "__main__":
@@ -159,32 +198,23 @@ if __name__ == "__main__":
     vecRMSprop = vec.copy()
 
     # momentum
-    for _ in range(100):
-        x_new, v_new = momentum(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
-        x = x_new
-        v = v_new
-        vec.append(np.linalg.norm(x))
 
-    print(f"Error: {np.linalg.norm(x_new) * 100}%")
+    x_opt, history = momentum(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
+    error_momentum = [np.linalg.norm(x) for x in history]
+    print(f"Error: {np.linalg.norm(x_opt) * 100}%")
 
     # Nestrov
     x, v = np.array([0.0, 2.0]), np.array([1.0, 1.0 ])
-    for _ in range(100):
-        x_new, v_new = NAG(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
-        x = x_new
-        v = v_new
-        vecNAG.append(np.linalg.norm(x))
+    x_opt, history= NAG(f, gradient, eta=0.1, beta=0.1, x=x, v=v)
+    error_NAG = [np.linalg.norm(x) for x in history]
 
     # Adagrad
     x = np.array([0.0, 2.0])
     G = 0
     eta = 0.1
     eps = 1e-8
-    for _ in range(100):
-        x_new, G_new = adagrad(f, gradient, G, eta, eps, x)
-        x = x_new
-        G = G_new
-        vecAdagrad.append(np.linalg.norm(x))
+    x_opt, history = adagrad(f, gradient, G, eta, eps, x)
+    error_adagrad = [np.linalg.norm(x) for x in history]
 
 
     # RMSprop
@@ -193,18 +223,15 @@ if __name__ == "__main__":
     eta = 0.1
     beta = 0.9
     eps = 1e-8
-    for _ in range(100):
-        x_new, G_new = RMSprop(f, gradient, G, eta, eps, beta, x)
-        x = x_new
-        G = G_new
-        vecRMSprop.append(np.linalg.norm(x))
+    x_opt, history = RMSprop(f, gradient, G, eta, eps, beta, x)
+    error_RMSprop = [np.linalg.norm(x) for x in history]
 
     # Plot results
     plt.figure()
-    plt.plot(vec)
-    plt.plot(vecNAG)
-    plt.plot(vecAdagrad)
-    plt.plot(vecRMSprop)
+    plt.plot(error_momentum)
+    plt.plot(error_NAG)
+    plt.plot(error_adagrad)
+    plt.plot(error_RMSprop)
     plt.xlabel("Iteration")
     plt.ylabel("Error")
     plt.legend(["momentum","Nestrov","Adagrad","RMSprop"])
