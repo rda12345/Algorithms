27e0d1ede534c7bc87e2c9201c2af0ae83980059
rda12345 <roiedann1987@gmail.com>
2026-01-12 21:54:14 +0200

    added simplex function

--- optimization/gradient_based_methods/quasi_newton.py
+++ optimization/gradient_based_methods/quasi_newton.py
@@ -6,20 +6,53 @@ the Hessian directly. Instead, they build an approximation of the Hessian,
 or its inverse and update this approximation using the gradient information only.
 They are much faster than Newton's method, and tend to converge faster.
 
-- BFGS: approximates the inverse of the Hessian. Widely used, stable and efficient.
-- L-BFGS: approximates the Hessian
+- BFGS: Approximates the inverse of the Hessian. Widely used, stable and efficient.
 
-Complexity: evaluation of the approximation of the inverse of the Hessian scales
+Key idea: The curvature of a function (encoded by the Hessian) can be approximated
+    by the change of the gradient. For a quadratic function the gradient change:
+    y_k = \nabla f(x_{k+1}) - \nabla f(x_{k}) = \nabla^2 f(x_{k}) s_k, where
+    s_k = x_{k+1} - x_{k}.
+    Therefore, the inverse of the Hessian at iteration H_{k+1} satisfies teh secant equation
+    H_{k+1} y_k = s_k. The algorithm enforces the secant equation, thus, enforcing
+    that the Hessian estimate behaves correctly along the direction moved (satisfies
+    the curvature constraint).
+    The update rule of the Hessian (is constructed in such a way such that it) satisfies the
+    secant condition, is symmetric and positive definite.
+    This leads to numerical stability and efficiency of the algorithm.
+
+Note: Many implementations of the BFGS determine the learning rate by a line search.
+    This improves the numerical stability and efficiency of the algorithm. The present
+    implementation assumes a small constant learning rate.
+
+- L-BFGS (Limited memory BFGS): Doesn't store the approximation of the inverse
+        Hessian, but only the last m curvature pairs. From the m pairs
+        the algorithm evaluates an inverse Hessian which equivalent to the
+        BFGS inverse Hessian restricted to the last m updates.
+
+Complexity: BFGS: Evaluation of the approximation of the inverse of the Hessian scales
             as O(n^2), in contrast to the standard Newton's method, which time-complexity
-            scales as O(n^3).
+            scales as O(n^3) (evaluating the inverse of a matrix scales as
+            matrix multiplication, so the n^3 can be improved applying divide and
+            conquer algorithm, such as the Strassen algorithm.).
+
+            Memory complexity of storing the approximation of the inverse of the Hessian,
+            H_{k}, scales as O(n^2).
+
+            For large n (e.g. machine learning applications) this becomes
+            infeasible.
+
+            L-BFGS only stores the last m curvature pairs (s_k, y_k). Therefore, the space
+            complexity of L-BFGS is O(m*n).
 """
-# TODO: fill up the complexity analysis
+import queue
+from collections import deque
 
 import numpy as np
 import jax.numpy as jnp
 from jax import grad
 import matplotlib.pyplot as plt
 from gradient_descent import gradient
+
 
 
 def f(x: np.array) -> float:
@@ -40,6 +73,35 @@ def BFGS_inv_hessian(
     """
     return (np.eye(len(s)) - rho * np.outer(s, y)) @ H @ (np.eye(len(s)) - rho * np.outer(y, s)) + rho * np.outer(s, s)
 
+def LBFGS_gradient_direction(
+        storage: list[tuple[np.ndarray, np.ndarray, float]],
+        g: np.ndarray,
+        m: int = 10,
+) -> np.ndarray:
+    q = g
+    alpha_list = []
+    for i in reversed(range(m)):
+        s, y, rho = storage[i]
+        alpha_i =  rho * np.outer(s, y)
+        alpha_list.append(alpha_i)
+        q -= alpha_i @ y
+
+    alpha_list = reversed(alpha_list)
+
+    s, y, _ = storage[m]
+    gamma = np.outer(s, y) / np.dot(y, y)
+    H0 = gamma * np.eye(len(s))
+    r = H0 @ q
+
+    for i in range(m):
+        s, y, rho = storage[i]
+        beta = rho * np.outer(y, r)
+        r += (a[i] - beta) * s
+
+    return r
+
+
+
 
 def BFGS(
         objective_function,
@@ -54,11 +116,6 @@ def BFGS(
 
     The method utilizes the gradient and an approximation of the inverse of the Hessian to optimize the
     objective function.
-
-    The learning rate of the optimization algorithm is determined by a line search, which ensures
-    the step actually reduces the objective, and that the approximation of the inverse of the
-    Hessian remains positive definite.
-    The learning rate should satisfy the Wolfe or strong Wolfe conditions.
 
     Args:
         objective_function
@@ -76,7 +133,6 @@ def BFGS(
     for _ in range(max_iter):
         g = gradient(objective_function, x)
 
-        # choose αk via line search
         p = H @ g
         x_new = x - eta * p
         history.append(x_new)
@@ -100,18 +156,74 @@ def BFGS(
 
 def LBFGS(
         objective_function,
-) -> np.ndarray:
-    pass
+        gradient: np.ndarray,
+        x: np.ndarray,
+        m:int = 10,
+        eta: float = 0.1,
+        max_iter: int = 100,
+        threshold: float = 1e-6
+) -> float:
+    """
+    L-BFGS (Limited memory BFGS) quasi-Newton method
+
+    The method utilizes the gradient and an approximation of the inverse of the Hessian to optimize the
+    objective function. In contrast to the BFGS, L-BFGS does not store an approximation of the
+    inverse of the Hessian, but evaluates it on the fly, utilizing m latest curvature pairs.
+
+    Args:
+        objective_function
+        gradient (np.ndarray): gradient of the objective function at point x
+        x (np.ndarray): initial search point
+        m (int): maximum number of curvature pairs forming the inverse Hessian approximation
+        eta (float): learning rate
+        max_iter (int): maximum number of iterations
+        threshold (float): convergence threshold
+
+    Returns:
+        float: optimal value
+    """
+    history = []
+    storage = []
+    q = queue.PriorityQueue()
+    H = np.eye(len(x))  # initialization of the approximation of the inverse Hessian
+    g = gradient(objective_function, x)
+    p = H @ g
+    for _ in range(max_iter):
+
+        x_new = x - eta * p
+        history.append(x_new)
+
+        s = x_new - x
+        y = gradient(objective_function, x_new) - g
+        ys = np.dot(y, s)
+        rho = 1.0 / ys
+
+        if np.linalg.norm(x_new - x) < threshold or ys <= 1e-10:
+            return x_new, history
+        x = x_new
+
+        # updating the queue
+        if len(q) < m:
+            q.push((s, y, rho))
+        else:
+            q.pop()
+            q.push((s, y, rho))
+
+        # evaluating the gradient direction
+        p = LBFGS_search_direction(storage=q.tolist(), g=g, m=m)
+
+
+    return x_new, history
 
 if __name__ == "__main__":
     x = np.array([2.0, 3.0])
-    x_opt, history = BFGS(f, gradient, x=x)
-    error = [np.linalg.norm(x - x_opt) for x in history]
-    print(f"Error: {np.linalg.norm(x_opt) * 100}%")
-
-    #x_opt, history = LBFGS(f, gradient, eta=0.1, x=x)
+    #x_opt, history = BFGS(f, gradient, x=x)
     #error = [np.linalg.norm(x - x_opt) for x in history]
-    #print(f"Error: {np.linalg.norm(x_opt)*100}%")
+    #print(f"Error: {np.linalg.norm(x_opt) * 100}%")
+
+    x_opt, history = LBFGS(f, gradient, eta=0.1, x=x)
+    error = [np.linalg.norm(x - x_opt) for x in history]
+    print(f"Error: {np.linalg.norm(x_opt)*100}%")
 
     plt.figure()
     plt.plot(error)
---
+++ optimization/linear_programming/simplex_method.py
@@ -0,0 +1,200 @@
+"""
+Simplex Method
+
+Algorithm to solve linear programing (LP) optimization problems.
+An LP is an optimization problem where the optimization function and
+constraints are linear equations/inequalities:
+
+maximize_{x_1,...,x_n}        \sum_i c_i x_i,
+subject to \{\sum_j a_{ij} x_j <= b_i\}
+            x_1,...,x_n >= 0.
+
+slack form: convert the first kind of inequalities above to equalities
+    by introducing slack variables.
+
+    x_{n+i} = b_j - \sum_j a_{ij} x_j
+    x_{n+i} > 0
+
+    Since the original inequality is true only if and only if the second
+    equality/inequality are true, the original and slack formulations of the
+    LP are equivalent.
+
+    In the slack form, the variables on the l.h.s are called basic variables,
+    and the those on the r.h.s of the equalities are called non-basic variables.
+    We also express the optimization function as z =\nu + \sum_i c_i x_i, where
+    \nu is a constant (doesn't influence the optimization).
+
+    As the algorithm proceeds the basic and non-basic variables change, but
+    the number of basic/non-basic variables does not change.
+
+The algorithm: The basic operation is a "pivot", exchanging a basic and
+non-basic variables.
+This is achieved by finding a basic feasible solution (a feasible solution, where
+all the non-basic variables are set to zero. Choose a variable in the optimization
+function which coefficient is positive, this is the "entering" variable
+and increase as much as possible, without violating the constraints
+(the basic variables must be non-negative).
+The leaving variable is the basic variable which limits the increase of
+the entering variable.
+
+The algorithm performs repeated pivots until all the coefficients of the
+optimization function are negative. Setting the variables in the optimization
+function to zero, gives the maximum value and the value of the basic variables
+is the optimal solution.
+
+Complexity: Exponential in the number of variables, constraints but
+typically can be solved in polynomial for standard cases.
+"""
+from cmath import inf
+
+import numpy as np
+from numpy.ma.core import nonzero
+
+
+def pivot(
+        N: list,
+        B: list,
+        A: np.ndarray,
+        b: np.ndarray,
+        c: np.ndarray,
+        nu: float,
+        l: int,
+        e: int,
+) -> tuple:
+    """
+    Performs a pivot in the simplex method.
+    Assumes the LP is already in slack form.
+
+    Args:
+        N (set): contains the indices of the non-basic variables.
+        B (set): contains the indices of the basic variables.
+        A (np.ndarray): contains the coefficients of the constraints.
+        b (np.ndarray): contains the bounds
+        c (np.ndarray): optimization coefficients
+        nu (float): optimization function constant
+        l (int): leaving variable (a basic variable)
+        e (int): entering variable (a non-basic variable)
+    """
+    A_new = np.zeros_like(A)
+    b_new = np.zeros_like(b)
+    c_new = np.zeros_like(c)
+
+    # Compute the coefficients of the equation for a new basic variable
+    b_new[e] = b[l]/A[l,e]
+    for j in N:
+        if j != e:
+            A_new[e,j] = A[l,j] / A[l,e]
+            print(A[l,e])
+    A_new[e,l] = 1 / A[l,e]  # l is in B not in N so it is not included in the sum
+
+    # Compute the coefficients of the remaining constraints
+    for i in B:
+        if i != l:
+            b_new[i] = b[i] - A[i,e] * b_new[e]
+            for j in N:
+                if j != e:
+                    A_new[i,j] = A[i,j] - A[i,e] * A_new[e,j]
+            A_new[i,l] = - A[i,e] * A_new[e,l]
+
+    # Compute the objective function
+    nu_new = nu + c[e] * b_new[e]
+    for j in N:
+        if j != e:
+            res = c[j] - c[e] * A_new[e,j]
+            c_new[j] = res
+    c_new[l] = - c[e] * A_new[e,l]
+
+    # Compute the new sets of basic and non-basic variables
+    N_new = (N - {e}) | {l}
+    B_new = (B - {l}) | {e}
+
+    return N_new, B_new, A_new, b_new, c_new, nu_new
+
+def initialize_simplex(
+        A: np.ndarray,
+        b: np.ndarray,
+        c: np.ndarray,
+) -> tuple:
+    n = len(A)
+    m = len(nonzero(b)[0])
+    N = {i for i in range(n-m)}
+    B = {i + m for i in range(n-m)}
+    nu = 0
+    return N, B, A, b, c, nu
+
+
+# TODO: fix bug in the code
+def simplex(
+        A: np.ndarray,
+        b: np.ndarray,
+        c: np.ndarray,
+) -> np.ndarray:
+    """
+    Simplex Method
+
+    Solves a Linear Program in slack form.
+    The choice of entering variables randomized, as well as the leaving
+    variable when there is a degeneracy.
+
+    Args:
+        A (np.ndarray): coefficients of the constraints.
+        b (np.ndarray): bounds of the constraints.
+        c (np.ndarray): optimization coefficients
+
+    Returns:
+        tuple  (np.ndarray, float): solution of the linear program
+                        and optimal (maximal) value.
+    """
+    N, B, A, b, c, nu = initialize_simplex(A, b, c)
+    print(f'N: {N}, B: {B}, A: {A}, b: {b}, c: {c}')
+    n = len(b)
+    Delta = np.zeros(n)
+    # from j in N choose j, such that c_j > 0
+    while np.any(c[list(N)] > 0):
+        positive_indices = np.where(c > 0)[0]
+        # Choose one of the indicies randomly
+        e = np.random.choice(positive_indices)
+        for i in B:
+            if A[i,e] > 0:
+                Delta[e] = b[i] / A[i,e]
+            else:
+                Delta[e] = inf
+        minimum_indices = np.where(Delta == np.min(Delta))[0]
+        l = np.random.choice(minimum_indices)
+        if Delta[l] == inf:
+            return 'unbounded'
+        else:
+            N, B, A, b, c, nu = pivot(N, B, A, b, c, nu, l, e)  #perform a pivot
+    x = np.zeros(n)  # initializing the solution array
+    for i in range(n):
+        if i in B:
+            x[i] = b[i]
+    return x, nu
+
+
+if __name__ == "__main__":
+
+    N = {0,1,2}
+    B = {3,4,5}
+    n = 6
+    temp = np.array([[1, 1, 3], [2, 2, 5], [4, 1, 2]]).astype(float)
+    A = np.zeros((n,n)).astype(float)
+    A[3:,:3] = temp
+    b = np.array([ 0, 0, 0, 30, 24, 36]).astype(float)
+    c = np.array([3, 1, 2, 0, 0, 0]).astype(float)
+
+
+    # converting the array elements to floats
+    A = A
+    nu = 0.0
+    l = 5
+    e = 0
+
+    # Pivot test
+    #N_new, B_new, A_new, b_new, c_new, nu = pivot(N=N, B=B, A=A, b=b, c=c, nu=nu, l=l, e=e)
+
+    # Simplex test
+    solution, optimal_value = simplex(A, b, c)
+
+    print(f'Optimal solution: {solution}')
+    print(f'Optimal value: {optimal_value}')
