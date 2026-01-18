4d3602ebec8e362862c1f3687e729ce5d01d289c
rda12345 <roiedann1987@gmail.com>
2026-01-18 23:16:05 +0200

    added an iterative fft algo

--- fft.py
+++ fft.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-An implementation of the radix-2 recursive Cooley–Tukey FFT algorithm.
-Assumes the length of the input array is a power of 2.
+Three implementation of the fast fourier transform (FFT) algorithm.
+1. Radix-2 recursive Cooley–Tukey fft algorithm.
+2. Iterative fft (has a lower constant in the actual running time
+                    not necessarily faster, depending on how the hardware
+                    uses the cache).
+
+A fft-shift function is also implemented, allowing easy plotting of the results.
+
+The algorithms assume that the length of the input array is a power of 2.
 Input and output are numpy arrays. 
 
-This is a divide and conquer algorithm, where the recursion relation is based
+The fft algorithm is a  divide and conquer method, where the recursion relation is based
 on a separation to two subroutines on the even and odd indices, respectively,
 and a reuse of the evaluated results. 
 
-Complexity: O(N*log(N))
+The fft (similarly DFT) is essentially a transformation between the coefficient
+representation and the point-value representation of polynomials, where
+the points of the polynomials are (n) complex roots of unity. The inverse transform
+interpolates, mapping the n roots to the coefficients of teh polynomial.
+Utilizing the fft algorithm allows multiplying polynomials in O(n*log(n)) (convolution theorem),
+by performing an fft and then multiplying the f(x_i) values of each point, takes O(n),
+and transforming back to the coefficient representation.
+
+A parallel implementation of the fft algorithm utilizing specific circuit design
+allows evaluating the fft with a circuit depth of O(log(n)).
+
+The inverse transformation is obtained by taking a complex conjugate twiddle factor.
+
+
+Complexity: O(n*log(n)), where n is the length of the coefficient vector.
 """
 import numpy as np
 
 
-def fft(x):
+def recursive_fft(x):
     """
     Computes the Discrete Fourier Transform (DFT) of x 
     using the recursive Cooley–Tukey FFT algorithm. Input and output are numpy arrays.
@@ -25,17 +46,17 @@ def fft(x):
     Returns:
         X: np.ndarray: DFT of x     
     """
-    N = len(x)  # array length
+    n = len(x)  # array length
     
     # base case
     if len(x)==1: return x
 
     
-    E = fft(x[::2])  # fft on all even elements of x
-    O = fft(x[1::2]) # fft on all odd elements of x
+    E = recursive_fft(x[::2])  # fft on all even elements of x
+    O = recursive_fft(x[1::2]) # fft on all odd elements of x
     
-    k = np.arange(0,N//2)     # fourier index vector
-    W = np.exp(-1j*2*np.pi*k/N) # phases
+    k = np.arange(0,n//2)     # fourier index vector
+    W = np.exp(-1j*2*np.pi*k/n) # twiddle factor
     
     X_low = E + W*O     # DFT of elements k = [0,...,N/2-1] (using elementwise multipication of the phases and odd terms)
     X_high = E - W*O    # DFT of elements k = [N/2,...,N] 
@@ -64,13 +85,55 @@ def fftshift(x):
     N = len(x)
     return np.concatenate((x[N//2:],x[:N//2]))
 
-#TODO: complete
+# TODO: fix the bug in iterative_fft
 def iterative_fft(x):
-    pass
+    A = bit_reverse_copy(x)  # A is a numpy array of length n in bit-reversed permutation order
+    n = len(x)      # n is a power of 2
+    for s in range(1,n.bit_length()):
+        m = 2**s
+        omega_m = np.exp(-1j*2*np.pi/m)     # twiddle factor
+        for k in range(0,n,m):
+            omega = 1
+            for j in range(m//2):
+                t = omega * A[k + j + (m//2)]
+                u = A[k + j]
+                A[k + j] = u + t
+                A[k + j + (m//2)] = u - t
+                omega = omega * omega_m
 
-#TODO: complete
-def bit_reverse_copy(x, X):
-    pass
+    return A
+
+def bit_reverse_copy(x):
+    """
+    Bit reverses the element of an array.
+
+    Args:
+        x: np.ndarray, input array
+
+    Returns:
+        np.ndarray, bit reversed array of type complex
+    """
+    n = len(x)
+    A = np.zeros(len(x), dtype=complex)
+    m = (n-1).bit_length()  # number of bits in the greatest element of x
+    for k in range(n):
+        A[rev(k,m)] = x[k]
+    return A
+
+def rev(k, n):
+    """
+    Reverses the bits of an int.
+
+    Args:
+        k: int, the number to be bit reversed
+        n: int, number of bits
+    """
+    rev = 0
+    for _ in range(n):
+        rev = (rev << 1) | k & 1
+        k >>= 1
+    return rev
+
 
 
 ## Test
@@ -82,17 +145,30 @@ if __name__ == "__main__":
     x = np.linspace(-L,L,N)
     k = np.arange(-N/2,N/2)*np.pi/L
     y = np.exp(-(x**2)/(2*sig**2))/(np.sqrt(2*np.pi*sig**2))
-    
-    fft = fft(y)
+
+
+
+    fft = recursive_fft(y)
     shifted_fft = fftshift(fft) 
     plt.plot(k,np.abs(shifted_fft))
     plt.xlabel("k")
     plt.ylabel("|fft(x)|")
     plt.title("Absolute value of the DFT of a Gaussian")
     plt.grid(True)
-    plt.show()
-    
-    
-    
+    #plt.show()
+
+    print('------------- TESTS -------------')
+
+    ## Bit reversal test
+    a = np.array(list(range(8)))
+    a_new = bit_reverse_copy(a)
+    a_res = np.array([0, 4, 2, 6, 1, 5, 3, 7])
+    print(f"Bit reversal test: {np.array_equal(a_new, a_res)}")
+
+    ## Iterative fft test
+    iter_fft = iterative_fft(y)
+
+    print(f"Iterative fft check: {np.max(np.imag(iter_fft-fft)) < 1e-14}")
+
     
     
